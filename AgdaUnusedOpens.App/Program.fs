0789ff764334a2805802e0a991d9fe1c434241eb
Smaug123 <patrick+github@patrickstevens.co.uk>
2020-01-04 14:43:12 +0000

    WIP

--- AgdaUnusedOpens.App/Program.fs
+++ AgdaUnusedOpens.App/Program.fs
@@ -1,11 +1,35 @@
-﻿open System
+﻿open AgdaUnusedOpens
+open System.IO
+
+let rec unusedOpens (config : AgdaCompiler) (opens : OpenStatement list) (file : AgdaFile) : OpenStatement seq =
+    seq {
+        for o in opens do
+            let f = OpenStatement.remove file o
+            if AgdaCompiler.compiles config f then
+                yield o
+    }
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
-
     // TODO - get the complete dependency graph by getting all the agda files
     // Then from the leaves inwards, get all their open statements;
     // try compiling without each one.
     // Finally write out the file with unnecessary open statements removed.
+
+    let config =
+        {
+            AgdaRoot = DirectoryInfo "/Users/Patrick/Desktop/Agda"
+            Compiler = FileInfo "/usr/local/bin/agda"
+        }
+
+    let graph = Graph.load config
+    printfn "%+A" graph
+
+    let f =
+        File.ReadAllLines "/Users/Patrick/Desktop/Agda/Numbers/Naturals/EuclideanAlgorithm.agda"
+        |> Array.toList
+        |> AgdaFile.make
+    let opens = OpenStatement.get f
+
+    printfn "%+A" (unusedOpens config opens f |> Seq.map (fun i -> i.LineNumber) |> Seq.toList)
+    0
--- AgdaUnusedOpens/Graph.fs
+++ AgdaUnusedOpens/Graph.fs
@@ -13,21 +13,40 @@ type Arrow =
 
 type Graph = Graph of Arrow list
 
+type ParseError =
+    | NotDotFile of Path * string
+    | FailedToCompile of Path
+
+[<RequireQualifiedAccess>]
 type ParseResult =
-    | NotDotFile of string
+    | Error of ParseError list
     | Success of Graph
 
 [<RequireQualifiedAccess>]
 module Graph =
 
+    let merge (Graph g1) (Graph g2) : Graph =
+        g1 @ g2
+        |> List.distinct
+        |> Graph
+
+    [<RequireQualifiedAccess>]
+    module internal ParseResult =
+        let merge (res1 : ParseResult) (res2 : ParseResult) =
+            match res1, res2 with
+            | ParseResult.Error e, ParseResult.Error f -> ParseResult.Error (e @ f)
+            | ParseResult.Error e, _ -> ParseResult.Error e
+            | _, ParseResult.Error f -> ParseResult.Error f
+            | ParseResult.Success g, ParseResult.Success h -> merge g h |> ParseResult.Success
+
     /// Given the contents of a dotfile, parse it.
-    let parse (dot : string) : ParseResult =
+    let parse (dot : string) : Result<Graph, string> =
         let dot = dot.Trim ()
         if not <| dot.StartsWith "digraph dependencies {" then
-            NotDotFile <| sprintf "Unexpected start of string: %s" dot
+            Error <| sprintf "Unexpected start of string: %s" dot
         else
         if not <| dot.EndsWith "}" then
-            NotDotFile (sprintf "File did not end with a }: %s." dot)
+            Error (sprintf "File did not end with a }: %s." dot)
         else
         let dot = dot.Substring(String.length "digraph dependencies {").TrimStart ()
         let dot = dot.Remove(String.length dot - 1).TrimEnd ()
@@ -62,16 +81,20 @@ module Graph =
                 To = Map.find to' nodes
             })
         |> Graph
-        |> Success
+        |> Ok
 
     let load (agda : AgdaCompiler) (f : AgdaFile) : ParseResult =
         let tmp = Path.GetTempFileName ()
         use proc = new Process ()
         proc.StartInfo.FileName <- agda.Compiler.FullName
+        proc.StartInfo.WorkingDirectory <- agda.AgdaRoot.FullName
         proc.StartInfo.Arguments <- sprintf "%s --dependency-graph=%s" (Path.combine agda.AgdaRoot f.Path).FullName tmp
         let res = proc.Start ()
         assert (res = true)
         proc.WaitForExit ()
-        tmp
-        |> File.ReadAllText
-        |> parse
+        if proc.ExitCode = 0 then
+            match tmp |> File.ReadAllText |> parse with
+            | Ok g -> ParseResult.Success g
+            | Error err -> ParseResult.Error (ParseError.NotDotFile (f.Path, err) |> List.singleton)
+        else
+            ParseResult.Error (ParseError.FailedToCompile f.Path |> List.singleton)
